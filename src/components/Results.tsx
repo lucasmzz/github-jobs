a63d1088228cb063d61de47408c97cffa5b00703
lucasmzz <lucasderh@gmail.com>
2023-02-26 17:57:42 +0100

    [STORY-2] calc posting date

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -3,6 +3,21 @@ import { GlobeAltIcon } from "@heroicons/react/24/outline";
 import { ClockIcon } from "@heroicons/react/24/outline";
 
 const Results = ({ posts }) => {
+  const getDaysSincePost = (date) => {
+    const today = new Date();
+    const postDate = new Date(date);
+
+    let dateDiff = undefined;
+    if (postDate) {
+      dateDiff = Math.ceil(Math.abs(today - postDate) / (1000 * 3600 * 24));
+    }
+
+    if (dateDiff < 0) {
+      return;
+    }
+
+    return dateDiff + " days ago";
+  };
   return (
     <div className="grow">
       <div className="flex flex-col flex-nowrap gap-4">
@@ -36,8 +51,8 @@ const Results = ({ posts }) => {
                       {post.city}
                     </p>
                     <p className="text-2xs font-semibold flex flex-nowrap items-center gap-1">
-                      <ClockIcon className="pl-1 h-5 w-5 text-gray-300" />5 days
-                      ago
+                      <ClockIcon className="pl-1 h-5 w-5 text-gray-300" />
+                      {getDaysSincePost(post.created)}
                     </p>
                   </div>
                 </div>
